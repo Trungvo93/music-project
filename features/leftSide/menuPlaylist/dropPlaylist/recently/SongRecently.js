2a964ef3217eddbbffe57daa1e17577acd411d3a
Trungvo93 <100949580+Trungvo93@users.noreply.github.com>
2023-08-06 23:16:26 +0700

    tinh chỉnh lại

--- features/leftSide/menuPlaylist/dropPlaylist/recently/SongRecently.js
+++ features/leftSide/menuPlaylist/dropPlaylist/recently/SongRecently.js
@@ -6,6 +6,10 @@ import { AppContext } from "@/context/context";
 import useSWR from "swr";
 import Image from "next/image";
 import { PlayIcon } from "@/svg/svg";
+import { Button, Modal } from "antd";
+import Link from "next/link";
+import LoginComp from "@/features/header/login/LoginComp";
+
 const fetchRecentlySongs = async (urlGetRecentlySongs, accessToken) => {
   if (accessToken) {
     return await axios
@@ -19,6 +23,7 @@ const fetchRecentlySongs = async (urlGetRecentlySongs, accessToken) => {
 const SongRecently = async () => {
   const { state, dispatch } = useContext(AppContext);
   const [accessToken, setAccessToken] = useState();
+  const [openRemindLogin, setOpenRemindLogin] = useState(false);
   // Get accessToken
   useEffect(() => {
     // Perform localStorage action
@@ -31,6 +36,24 @@ const SongRecently = async () => {
     ([url, token]) => fetchRecentlySongs(url, token)
   );
 
+  const handleClickOpenRemindLogin = () => {
+    setOpenRemindLogin(true);
+  };
+
+  const handleClickCloseRemindLogin = () => {
+    setOpenRemindLogin(false);
+  };
+  useEffect(() => {
+    const checkLogged = localStorage.getItem("accessKey") ? true : false;
+    if (checkLogged === false) {
+      handleClickOpenRemindLogin();
+    }
+  }, [state.userLogged]);
+  useEffect(() => {
+    if (state.isLogin) {
+      handleClickCloseRemindLogin();
+    }
+  }, [state.isLogin]);
   const handlePlay = (item) => {
     const newArray = [item];
     const list = newArray.map((item, index) => {
@@ -82,6 +105,39 @@ const SongRecently = async () => {
             ))
           : ""}
       </div>
+      <Modal
+        closeIcon={null}
+        mask={false}
+        footer={null}
+        title={
+          <div className='flex justify-between items-center text-blue-700 border-b-2 border-stone-100 pb-2'>
+            <p className='text-blue-700'>{"Đăng nhập để sử dụng"}</p>
+            <Button
+              className='flex justify-center items-center'
+              type='text'
+              shape='circle'>
+              <Link href='/'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-5 h-5 hover:text-rose-500'>
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M6 18L18 6M6 6l12 12'
+                  />
+                </svg>
+              </Link>
+            </Button>
+          </div>
+        }
+        open={openRemindLogin}
+        onCancel={handleClickCloseRemindLogin}>
+        <LoginComp />
+      </Modal>
     </div>
   );
 };
